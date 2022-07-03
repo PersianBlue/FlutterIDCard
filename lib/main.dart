936ce373c421f3273358b4c69edbdafb200b0216
PersianBlue <71480723+PersianBlue@users.noreply.github.com>
2022-07-03 06:56:00 -0500

    made quotes into its own branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,55 @@
-import "package:flutter/material.dart";
-import 'package:flutter_apps/pages/choose_location.dart';
-import 'package:flutter_apps/pages/home.dart';
-import 'package:flutter_apps/pages/loading.dart';
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, prefer_const_constructors_in_immutables, use_key_in_widget_constructors, avoid_print
 
-void main() => runApp(MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Loading(),
-        '/home': (context) => const Home(),
-        '/location': (context) => const ChooseLocation(),
-      },
-    ));
+import 'package:flutter/material.dart';
+import 'package:flutter_apps/QuoteFiles/quote.dart';
+import 'package:flutter_apps/QuoteFiles/quote_card.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(
+        author: "Oscar Wilde",
+        text: "Be yourself: everyone else is already taken"),
+    Quote(
+        text: "I have nothing to declare except my genius",
+        author: "Oscar Wilde"),
+    Quote(
+        text: "The truth is rarely pure and never simple",
+        author: "Oscar Wilde")
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text("Awesome quotes"),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+            children: quotes.map((quote) {
+          return QuoteCard(
+              quote: quote,
+              padding: 24.0,
+              delete: () {
+                setState(() {
+                  quotes.remove(quote);
+                  print("Love");
+                });
+              });
+        }).toList()));
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -25,59 +25,62 @@ class HomeState extends State<Home> {
     return Scaffold(
       backgroundColor: bgColor,
       body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/$bgImage"),
-              fit: BoxFit.cover,
+        child: Center(
+          child: Container(
+            width: 500.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/$bgImage"),
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
-            child: Column(
-              children: [
-                TextButton.icon(
-                  onPressed: () async {
-                    dynamic result =
-                        await Navigator.pushNamed(context, "/location");
-                    if (result != null) {
-                      setState(() {
-                        data = {
-                          "time": result['time'],
-                          "location": result['location'],
-                          "isDayTime": result['isDayTime'],
-                          "flag": result["flag"],
-                        };
-                      });
-                    }
-                  },
-                  icon: Icon(
-                    Icons.edit_location,
-                    color: Colors.grey[700],
-                  ),
-                  label: Text('Edit Location',
-                      style: TextStyle(
-                        color: Colors.grey[300],
-                      )),
-                ),
-                SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(data["location"],
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+              child: Column(
+                children: [
+                  TextButton.icon(
+                    onPressed: () async {
+                      dynamic result =
+                          await Navigator.pushNamed(context, "/location");
+                      if (result != null) {
+                        setState(() {
+                          data = {
+                            "time": result['time'],
+                            "location": result['location'],
+                            "isDayTime": result['isDayTime'],
+                            "flag": result["flag"],
+                          };
+                        });
+                      }
+                    },
+                    icon: Icon(
+                      Icons.edit_location,
+                      color: Colors.grey[700],
+                    ),
+                    label: Text('Edit Location',
                         style: TextStyle(
-                            fontSize: 28.0,
-                            letterSpacing: 2.0,
-                            color: Colors.white))
-                  ],
-                ),
-                SizedBox(height: 20.0),
-                Text(data['time'],
-                    style: TextStyle(
-                      fontSize: 66.0,
-                      color: Colors.white,
-                    ))
-              ],
+                          color: Colors.grey[300],
+                        )),
+                  ),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(data["location"],
+                          style: TextStyle(
+                              fontSize: 28.0,
+                              letterSpacing: 2.0,
+                              color: Colors.white))
+                    ],
+                  ),
+                  SizedBox(height: 20.0),
+                  Text(data['time'],
+                      style: TextStyle(
+                        fontSize: 66.0,
+                        color: Colors.white,
+                      ))
+                ],
+              ),
             ),
           ),
         ),
